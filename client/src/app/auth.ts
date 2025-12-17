024700e2538459e458daf8ef928d887b08d39d93
Ayush <ayush.kumar.21cse@bmu.edu.in>
2025-12-17 20:35:03 +0530

    fixed the github Oauth

--- client/src/app/auth.ts
+++ client/src/app/auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-import GitHub from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github"
 
 export const {handlers, signIn, signOut, auth}=NextAuth({
-    providers:[Google, GitHub],
+    providers:[Google, GithubProvider({clientId:process.env.GITHUB_CLIENT_ID, clientSecret:process.env.GITHUB_CLIENT_SECRET})],
     secret:process.env.NEXTAUTH_SECRET
 })
