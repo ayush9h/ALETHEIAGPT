ec6c2558828faa8105914b5b5ecb250838a89452
Ayush <ayush.kumar.21cse@bmu.edu.in>
2026-01-31 12:44:43 +0530

    (add): added db pooling configs

--- server/app/db_service/db.py
+++ server/app/db_service/db.py
@@ -1,29 +1,20 @@
 from typing import AsyncGenerator
 
 from app.utils.config import settings
-from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlmodel import SQLModel
 
 engine = create_async_engine(
     settings.DB_POSTGRES_URL,
-    echo=False,
+    pool_size=20,
+    max_overflow=30,
+    pool_timeout=60,
 )
 
 async_session_maker = async_sessionmaker(
-    engine,
+    bind=engine,
     expire_on_commit=False,
-    class_=AsyncSession,
+    autoflush=False,
 )
-
-# ============== Connection Utils ===================
-async def _init_db():
-    async with engine.begin() as conn:
-        await conn.execute(text("SELECT 1"))
-        await conn.run_sync(SQLModel.metadata.create_all)
-
-async def _close_db():
-    await engine.dispose()
 
 
 # ============== Dependency ===================
